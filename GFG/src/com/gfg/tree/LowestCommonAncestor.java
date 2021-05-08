15b46727077532f77b6c951ada62b1f556ed66b7
abhicit <skd.monu716@gmail.com>
2021-05-09 01:42:58 +0530

    efficient find LCA

--- GFG/src/com/gfg/tree/LowestCommonAncestor.java
+++ GFG/src/com/gfg/tree/LowestCommonAncestor.java
@@ -15,6 +15,22 @@ class Node
 
 public class LowestCommonAncestor {
 	
+	public static Node findLCAEfficient(Node root, int n1, int n2) {//Theta(n)
+		if(root == null)
+			return null;
+		if(root.data == n1 || root.data == n2)
+			return root;
+		Node lca1 = findLCAEfficient(root.left, n1, n2);
+		Node lca2 = findLCAEfficient(root.right, n1, n2);
+		
+		if(lca1 != null && lca2 != null)
+			return root;
+		if(lca1 != null)
+			return lca1;
+		else
+			return lca2;
+	}
+	
 	public static Node findLCA(Node root, int n1, int n2) {
 		ArrayList<Node> path1 = new ArrayList<>();
 		ArrayList<Node> path2 = new ArrayList<>();
@@ -50,9 +66,10 @@ public class LowestCommonAncestor {
     	root.right=new Node(30);
     	root.right.left=new Node(40);
     	root.right.right=new Node(50);
-    	int n1=40,n2=50;
+    	int n1=20,n2=50;
 	
-	    Node ans=findLCA(root,n1,n2);
+	    //Node ans=findLCA(root,n1,n2);
+	    Node ans=findLCAEfficient(root,n1,n2);
     	System.out.println("LCA: "+ans.data);
     } 
 	
